b4d31c2a08d1584e88960bc1a5ba12dd4cfd0ea6
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-05-11 11:21:55 -0500

    login logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,14 +14,15 @@ router.post('/register', async(req, res, next) => {
 
 	//create an object for the db entry, the properties of this object
 	//will match up with our model
-
+	console.log(passwordHash, 'this is passwordHash');
+	
 	const userDbEntry = {};
 	userDbEntry.username = req.body.username;
 	userDbEntry.password = passwordHash;
 
 	try{
 
-		const createdUser = await User.create(req.body);
+		const createdUser = await User.create(userDbEntry);
 		req.session.logged =  true;
 		req.session.userDbId =  createdUser._id;
 
@@ -32,29 +33,63 @@ router.post('/register', async(req, res, next) => {
 			message: `Successfully created account for ${createdUser.username}`
 		}) 
 		
-
+	console.log(createdUser)
 	}catch(err){
 		next(err)
 	}
 })
 
-// router.post('/login', async(req, res, next) => {
+router.post('/login', async(req, res, next) => {
 
-// 	try{
-// 		const foundUser = await User.findOne({'username': req.body.username});
+	try{
+		const foundUser = await User.findOne({'username': req.body.username});
 
-// 		if(foundUser){
-// 			if(bcrypt.compareSync(req.body.password, foundUser.password) === true){
-// 				req.session.logged = true;
-// 				req.session.userDbId = foundUser._id;
-// 			}else{
+		if(foundUser){
+			if(bcrypt.compareSync(req.body.password, foundUser.password) === true){
+				req.session.logged = true;
+				req.session.userDbId = foundUser._id;
+
+				res.json({
+					status: 200,
+					isLegit: true,
+					data: foundUser,
+					message: `Thanks for sign in ${foundUser.username}`
+				})
+			}else{
 				
-// 			}
-// 		}
-// 	}catch(err){
-// 		console.log(err)
-// 	}
-// })
+				res.json({
+					status: 404,
+					islegit: false,
+					message: 'username or password is incorrect'
+				})
+			}
+		}else{
+				res.json({
+					status: 404,
+					isLegit: false,
+					message: 'username or password is incorrect'
+				})
+			}
+	}catch(err){
+		res(err)
+	}
+})
+
+
+	router.get('/logout', (req, res) => {
+		req.session.destroy((err)=>{
+			if(err){
+				res.json({
+					status: 404
+				})
+			}else{
+				res.json({
+					status: 200,
+					message: 'Thanks for visiting, comeback soon'
+				})
+			}
+		})
+	})
 
 
 module.exports = router

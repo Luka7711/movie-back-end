deb4779033d82edab5dc8271137f43d4033fbbb0
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-08-31 21:52:43 -0500

    test

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://movies-in-park.herokuapp.com/",
+  origin: "https://movies-in-park.herokuapp.com/",
   credentials: true,
   optionSuccessStatus: 200,
 };

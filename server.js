24101d50c594da3d34fb9a717d25581e65eddbde
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2021-09-02 15:09:57 -0500

    test

--- server.js
+++ server.js
@@ -24,21 +24,30 @@ console.log(PORT);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const corsOptions = {
-  origin: 'https://movies-in-park.herokuapp.com',
-  credentials: true,
-  optionSuccessStatus: 200
-};
+// const corsOptions = {
+//   origin: 'https://movies-in-park.herokuapp.com',
+//   credentials: true,
+//   optionSuccessStatus: 200
+// };
 
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
     'https://movies-in-park.herokuapp.com'
   );
+
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+
   next();
 });
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 const userController = require('./controllers/userController');
 const movieController = require('./controllers/movieController');

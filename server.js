c1a868e37543dbeceffe81407af219fc3d7e4e28
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-09-05 13:45:38 -0500

    added multiple domains for call

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 require("./db/db");
 
 const PORT = process.env.PORT;
+const domains = [
+  "https://movies-in-park.herokuapp.com",
+  "http://localhost:3000",
+];
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://movies-in-park.herokuapp.com"
-  );
+  let thisDomain = domains.indexOf(req.header("Origin"));
+  res.setHeader("Access-Control-Allow-Origin", domains[thisDomain]);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
